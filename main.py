c5fd1d666cb842697d69d4b72897bab15af2b5dd
ShyNeon55 <shy_neon@outlook.com>
2025-08-26 10:38:15 -0500

    Set the fps to 60 and got the player to spawn on the screen

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    while True:
+        #This keeps the game running
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            #This allows you to exit the game by hitting the exit button
+        dt = clock.tick(60) / 1000
+        #This sets the fps to 60 and sets the miliseconds to the delta
+        screen.fill((0, 0, 0))
+        #Makes the screen black
+        player.draw(screen)
+        #Redraws player each frame
+        pygame.display.flip()
+        
 
 
 if __name__ == "__main__":
